eb0a3ec4ba528ec8f871cd3d2d31a44fa06870e3
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-09-24 10:24:35 +0000

    test: some more endpoints exploration

--- api_tests.R
+++ api_tests.R
@@ -1,72 +1,253 @@
 # Let's test the endpoint to see which data is available
+# See https://www.chess.com/news/view/published-data-api for a list of endpoints
 
 # SETUP ------------------------------------------------------------------------
 
 # Import libraries
+library(tibble)
 library(dplyr)
+library(tidyr)
 library(stringr)
 library(purrr)
+library(magrittr)
 library(httr2)
+library(anytime)
 
 # TESTING API ENDPOINTS --------------------------------------------------------
 
 ## PLAYER API ------------------------------------------------------------------
 
-# perform API call
+# make a request to the player endpoint
 resp <- request("https://api.chess.com/pub/player/balcoo") %>% 
-  req_perform()
-
-# extract information on expiration from response header
-# this tells us when the data will refresh (I think...)
-expires <- resp %>% 
-  resp_header(header = 'expires') %>% 
-  as.POSIXct(format = '%a, %d %b %Y %X', tz = 'GMT')
-
-# extract the body of the response
-res <- resp %>% 
+  # perform API call
+  req_perform() %>% 
+  # extract the body of the response as a list
   resp_body_json() %>% 
-  # when converting to tibble, fix the "@id" column name
+  # convert to tibble, fix the "@id" column name
   bind_cols(.name_repair = make.names)
 
+# according to documentation, this are all available fields
+#   "@id": "URL", // the location of this profile (always self-referencing)
+#   "url": "URL", // the chess.com user's profile page (the username is 
+#       displayed with the original letter case)
+#   "username": "string", // the username of this player
+#   "player_id": 41, // the non-changing Chess.com ID of this player
+#   "title": "string", // (optional) abbreviation of chess title, if any
+#   "status": "string", // account status: closed, closed:fair_play_violations, 
+#       basic, premium, mod, staff
+#   "name": "string", // (optional) the personal first and last name
+#   "avatar": "URL", // (optional) URL of a 200x200 image
+#   "location": "string", // (optional) the city or location
+#   "country": "URL", // API location of this player's country's profile
+#   "joined": 1178556600, // timestamp of registration on Chess.com
+#   "last_online": 1500661803, // timestamp of the most recent login
+#   "followers": 17 // the number of players tracking this player's activity
+#   "is_streamer": "boolean", //if the member is a Chess.com streamer
+#   "twitch_url": "Twitch.tv URL",
+#   "fide": "integer" // FIDE rating
+
+# Additional notes:
+# - "fide" is NEVER sent, might be deprecated
+# - "twitch_url" is also OPTIONAL, despite not being labeled as such
+
+# for the optional fields, we create empty column if they are not present
+opt_cols <- c(
+  title      = NA_character_, 
+  name       = NA_character_, 
+  avatar     = NA_character_, 
+  location   = NA_character_,
+  twitch_url = NA_character_
+)
+
+# add empty columns for optional values not present in the response
+resp <- resp %>% 
+  add_column(!!!opt_cols[!names(opt_cols) %in% names(resp)])
+
 # some preprocessing to keep only relevant information
-res <- res %>%
-  # if the user doesn't have it, add a "twitch_url" column (empty)
-  { if("twitch_url" %in% colnames(.)) . else 
-    mutate(., twitch_url = NA_character_, .after = is_streamer) } %>% 
+resp <- resp %>%
   mutate(
-    # convert EPOCH timestamps to redable times
-    across(c(joined, last_online), anytime::anytime, tz = 'GMT'),
+    # convert EPOCH timestamps to readable times
+    across(c(joined, last_online), anytime, tz = 'GMT'),
     # remove unneeded URLs
     country = str_remove(country, '^https://api.chess.com/pub/country/'),
     twitch_url = str_remove(twitch_url, '^https://twitch.tv/'),
-    expires = expires
+    username = str_remove(url,'^https://www.chess.com/member/'),
+    .keep = 'unused'
   ) %>% 
-  select(-c(X.id, url))
+  # remove column(s) containing no useful information
+  select(
+    # this is just the endpoint called, no point in keeping it
+    -X.id
+  ) %>% 
+  # relocate columns to an order that makes more sense to me
+  relocate(
+    player_id,
+    username,
+    name,
+    joined,
+    last_online,
+    country,
+    location,
+    followers,
+    status,
+    is_streamer,
+    twitch_url,
+    verified,
+    title,
+    avatar
+  )
 
-# TITLED PLAYERS ---------------------------------------------------------------
+## TITLED PLAYERS --------------------------------------------------------------
 
-# perform API call
+# make a request to the titled player endpoint
 # valid titles: GM, WGM, IM, WIM, FM, WFM, NM, WNM, CM, WCM
 resp <- request("https://api.chess.com/pub/titled/GM") %>% 
-  req_perform()
-
-# extract the body of the response
-res <- resp %>% 
+  # perform API call
+  req_perform() %>% 
+  # extract the body of the response as a list
   resp_body_json() %>% 
+  # the response is an array, so convert it to the equivalent R object
   unlist() %>% 
   unname()
 
-# PLAYER STATS -----------------------------------------------------------------
+### COMBINE TITLED PLAYERS AND PLAYER API --------------------------------------
 
-# There's a lot of stuff in here; need to find a smart way to store this info
-# That's work for another day
+# define a function to fetch data from a player api request
+# this is the same as what was done above when calling the player endpoint
+get_player_profile <- function(player){
+  
+  # make a request to the player endpoint
+  resp <- request(paste0("https://api.chess.com/pub/player/", player)) %>% 
+    req_perform() %>% 
+    resp_body_json() %>% 
+    bind_cols(.name_repair = make.names)
+  
+  # for the optional fields, we create empty column if they are not present
+  opt_cols <- c(
+    title      = NA_character_, 
+    name       = NA_character_, 
+    avatar     = NA_character_, 
+    location   = NA_character_,
+    twitch_url = NA_character_
+  )
+  
+  # add empty columns for optional values not present in the response
+  resp <- resp %>% 
+    add_column(!!!opt_cols[!names(opt_cols) %in% names(resp)])
+  
+  # some preprocessing to keep only relevant information
+  resp <- resp %>%
+    mutate(
+      across(c(joined, last_online), anytime, tz = 'GMT'),
+      country = str_remove(country, '^https://api.chess.com/pub/country/'),
+      twitch_url = str_remove(twitch_url, '^https://twitch.tv/'),
+      username = str_remove(url,'^https://www.chess.com/member/'),
+      .keep = 'unused'
+    ) %>% 
+    select(
+      -X.id
+    ) %>% 
+    relocate(
+      player_id,
+      username,
+      name,
+      joined,
+      last_online,
+      country,
+      location,
+      followers,
+      status,
+      is_streamer,
+      twitch_url,
+      verified,
+      title,
+      avatar
+    )
+  
+  # return the response, after preprocessing it
+  return(resp)
+  
+}
 
-# perform API call
+# # make a player profile API call to each of the titled players
+# resp <- map_dfr(
+#   .x = resp, # vector with the list of titled players
+#   .f = get_player_profile
+# )
+# this takes a few minutes to run for some reason, so let's leave it commented... 
+
+## PLAYER STATS ----------------------------------------------------------------
+
+# make a request to the player stats endpoint
 resp <- request("https://api.chess.com/pub/player/balcoo/stats") %>% 
-  req_perform()
-
-# extract the body of the response
-res <- resp %>% 
+  # perform API call
+  req_perform() %>% 
+  # extract the body of the response as a list
   resp_body_json()
 
-View(res)
+# There's a lot of stuff in here; need to find a smart way to store this info
+# Will come back to this later on
+
+## PLAYER ONLINE STATUS --------------------------------------------------------
+
+# make a request to the online status endpoint
+resp <- request("https://api.chess.com/pub/player/balcoo/is-online") %>% 
+  # perform API call
+  req_perform()
+
+# This returns an error 404
+# The official examples are also not working...
+# This endpoint might be deprecated ?
+
+## PLAYER GAMES ----------------------------------------------------------------
+
+# As per documentation, there are 5 different endpoints available:
+# - Current Daily Chess: daily Chess games that a player is currently playing;
+# - To-Move Daily Chess: daily Chess games where it is the player's turn to act;
+# - List of Monthly Archives: monthly archives available for this player;
+# - Complete Monthly Archives: Live and Daily Chess games that a player finished;
+# - Multi-Game PGN Download: standard multi-game PGN with all games for a month.
+
+### CURRENT DAILY CHESS --------------------------------------------------------
+
+# make a request to the player stats endpoint
+resp <- request("https://api.chess.com/pub/player/tuhieu/games") %>% 
+  # perform API call
+  req_perform() %>% 
+  # extract the body of the response as a list
+  resp_body_json() %>% 
+  # for some reason, the response is a list within a list; extract it
+  extract2('games') %>% 
+  # convert the list with all info for each game, to a tibble
+  map_dfr(bind_cols)
+
+# there are 2 optional fields:
+# - tournament: only for tournament matches, link to the tournament
+# - match : only for team matches, link to the team match
+
+# HOW TO ADDRESS THESE?
+
+# preprocessing
+respp %>%
+  mutate(
+    # remove URL from game ID (this also contains the game time format!)
+    game_id = str_remove(url, pattern = "^https://www.chess.com/game/daily/"),
+    # remove URL from tournament name
+    tournament = str_remove(tournament, "^https://api.chess.com/pub/tournament/"),
+    # remove URL from team match
+    team_match = str_remove(match, "^https://api.chess.com/pub/match/"),
+    # convert EPOCHs to readable timestamps
+    across(c(move_by, last_activity, start_time), anytime, tz = 'GMT'),
+    # remove URL from player IDs
+    across(
+      .cols = c(white, black), 
+      .fns = ~str_remove(., pattern = "^https://api.chess.com/pub/player/"),
+      .names = 'player_{col}'
+    ),
+    .keep = 'unused'
+  ) %>% 
+  View()
+
+### TO-MOVE DAILY CHESS --------------------------------------------------------
+
+# TBD
